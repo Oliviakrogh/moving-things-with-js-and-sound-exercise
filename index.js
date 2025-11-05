262a1947febe9b386e01303230eedf7f66e5882e
Oliviakrogh <eaa25olkr@students.eaaa.dk>
2025-11-05 13:32:21 +0100

    opgave 3

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function moveDodgerLeft() {
   const left = parseInt(leftNumbers, 10);
 
   if (left > 0) {
-    dodger.style.left = `${left - 1}px`;
+    dodger.style.left = `${left - 5}px`;
   }
 }
 function moveDodgerRight() {
@@ -16,7 +16,25 @@ function moveDodgerRight() {
 
   if (left < 360) {
     // 400px bred bane minus 40px bred dodger fx
-    dodger.style.left = `${left + 1}px`;
+    dodger.style.left = `${left + 5}px`;
+  }
+}
+
+function moveDodgerUp() {
+  const bottomNumbers = dodger.style.bottom.replace("px", "");
+  const bottom = parseInt(bottomNumbers, 10);
+
+  if (bottom < 360) {
+    dodger.style.bottom = `${bottom + 5}px`;
+  }
+}
+
+function moveDodgerDown() {
+  const bottomNumbers = dodger.style.bottom.replace("px", "");
+  const bottom = parseInt(bottomNumbers);
+
+  if (bottom < 360) {
+    dodger.style.bottom = `${bottom - 5}px`;
   }
 }
 
@@ -26,5 +44,11 @@ document.addEventListener("keydown", function (e) {
   }
   if (e.key === "ArrowRight") {
     moveDodgerRight();
+  }
+  if (e.key === "ArrowUp") {
+    moveDodgerUp();
+  }
+  if (e.key === "ArrowDown") {
+    moveDodgerDown();
   }
 });
