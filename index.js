9ebdc9cba5f37a5a1fe80ac203168798f2006c15
Oliviakrogh <eaa25olkr@students.eaaa.dk>
2025-11-05 13:08:16 +0100

    Exercise 1

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+"use strict";
+
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    // 400px bred bane minus 40px bred dodger fx
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
